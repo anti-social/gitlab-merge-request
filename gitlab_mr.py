334b8bd95bd8ae467f1ba18678cf3998ae6c1f46
Alexander Koval <kovalidis@gmail.com>
2016-12-23 22:44:50 +0200

    Bump version to 0.1.1

--- gitlab_mr.py
+++ gitlab_mr.py
@@ -13,7 +13,7 @@ import git
 from gitlab import Gitlab, GitlabError, GitlabGetError, GitlabConnectionError
 
 
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 
 
 log = logging.getLogger('gitlab-cli')
