63d877fda129ce05386900440582782d76e5005a
Alexander Koval <kovalidis@gmail.com>
2017-01-24 18:56:28 +0200

    Bump version to 0.2.1

--- gitlab_mr.py
+++ gitlab_mr.py
@@ -15,7 +15,7 @@ from gitlab import Gitlab, GitlabError, GitlabGetError, GitlabConnectionError
 import colorama
 
 
-__version__ = '0.2.0'
+__version__ = '0.2.1'
 
 
 log = logging.getLogger('gitlab-cli')
